05e23d8827f824177d0de305d288465f70b14c8b
Daniel <danielfbz10@gmail.com>
2023-05-30 07:54:35 -0500

    Challenger Encriptador finalizado

--- encriptador.js
+++ encriptador.js
@@ -9,14 +9,12 @@ function encriptar() {
     let mensajeAlerta = document.getElementById("mensajeAlerta");
     let textoResultado = document.getElementById("textoResultado");
     let botonCopiar = document.getElementById("btn-copiar");
-    
-
 
     let textoEncriptado = texto.replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat")
     textoResultado.textContent = textoEncriptado;
     munieco.style.display = 'none';
     mensaje.style.display = 'none';
@@ -31,14 +29,14 @@ function desencriptar() {
     let munieco = document.getElementById("munieco");
     let mensaje = document.getElementById("mensaje");
     let mensajeAlerta = document.getElementById("mensajeAlerta");
-    let textoResultado = document.getElementById("textoResultado"); 
+    let textoResultado = document.getElementById("textoResultado");
     let botonCopiar = document.getElementById("btn-copiar");
 
     let textoDesencriptado = texto.replace(/enter/gi, "e")
-    .replace(/imes/gi, "i")
-    .replace(/ai/gi, "a")
-    .replace(/ober/gi, "o")
-    .replace(/ufat/gi, "u");
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
 
     textoResultado.textContent = textoDesencriptado;
     munieco.style.display = 'none';
@@ -46,8 +44,15 @@ function desencriptar() {
     mensajeAlerta.style.display = 'none';
     botonCopiar.style.display = 'block';
     botonCopiar.style.alignItems = 'center'
+}
 
-
+function copiar (){
+    const btnCopiar = document.getElementById("btn-copiar");
+    btnCopiar.addEventListener("click", copiar = () => {
+        var contenido = document.getElementById("textoResultado").textContent;
+        navigator.clipboard.writeText(contenido);
+        
+    })
 }
 
 
